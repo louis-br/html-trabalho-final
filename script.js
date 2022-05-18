ee2b9cd007eefaf0c24b7b38b6e026222eefe875
DIEGO HENRIQUE ARENAS OKAWA <diego.arenas1505@gmail.com>
2022-05-18 17:54:26 -0300

    Dados cambio

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
-function alertaMensagem(texto){
+function alertaMensagem(texto) {
+
     alert(texto);
+    
+}
+
+function tabela() {
+
+    console.log('Entrou')
+
+    for(let k = 0; k <= 10; k++){
+
+        let item = document.createElement('li');
+        item.innerHTML = '7 x ' + k + ' = ' + (k * 7);
+        document.getElementById('lista').appendChild(item);
+
+    }
+
+}
+
+function testeFuncao() {
+
+    console.log('Teste')
+
+    let list = document.getElementById('lista')
+    let li = document.createElement('li')
+    li.innerHTML = '<b> ola </b>'
+    list.appendChild(li)
+
 }
