40b7570147282af9f797636556e2f80e44963817
diegookawa <diego.arenas1505@gmail.com>
2022-05-23 18:15:36 -0300

    Adicionar tabela estática

--- dadosCambio/dadosCambio.js
+++ dadosCambio/dadosCambio.js
@@ -1,28 +1,29 @@
-var requestURL = "https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoMoedaDia(moeda=@moeda,dataCotacao=@dataCotacao)?'@moeda=''&@dataCotacao=''&$format=json"
+var requestURL = "https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoMoedaPeriodo(moeda=@moeda,dataInicial=@dataInicial,dataFinalCotacao=@dataFinalCotacao)?@moeda='EUR'&@dataInicial='04-07-2021'&@dataFinalCotacao='07-20-2021'&$top=10000&$skip=0&$format=json&$select=cotacaoVenda,dataHoraCotacao"
 var request = new XMLHttpRequest()
 
 request.open('GET', requestURL)
-request.responseType('json')
+request.responseType = 'json'
 request.send()
 request.onload = function() {
 
+    console.log(request.response)
     mostrarTabela(request.response)
 
 }
 
-function mostrarTabela (c) {
+function mostrarTabela (v) {
 
     let tabela = document.getElementById('tabela')
 
-    for (k = 0; k < c.length; k++) {
+    for (k = 0; k < v.value.length; k++) {
 
         let tr = document.createElement('tr')
         tabela.appendChild(tr)
         let td = document.createElement('td')
-        td.innerText = c[k].dataHoraCotacao
+        td.innerText = v.value[k].dataHoraCotacao
         tr.appendChild(td)
         td = document.createElement('td')
-        td.innerText = c[k].cotacaoCompra
+        td.innerText = v.value[k].cotacaoVenda
         tr.appendChild(td);
 
     }
