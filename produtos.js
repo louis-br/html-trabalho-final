26371665c6cbce95f51689ed8a9e17e9e16ca7e5
louis-br <46930119+louis-br@users.noreply.github.com>
2022-06-27 17:19:15 -0300

    Adicionar carrinho

--- produtos.js
+++ produtos.js
@@ -1,3 +1,5 @@
+let produtos = {}
+
 var requestCategoriasURL = 'http://loja.buiar.com/?key=8t4b2j&c=categoria&t=listar&f=json'
 
 var requestCategorias = new XMLHttpRequest()
@@ -101,12 +103,24 @@ function gerarProdutos (response, tipo, idCategoria) {
 
     for (let i = 0; i < dados.length; i++) {
 
+        {
+            let id = Number(dados[i].id)
+            produtos[id] = dados[i]
+        }
+
         if(dados[i].categoria == idCategoria) {
 
             let div = document.createElement('div')
             let image = document.createElement('img')
             let p = document.createElement('p')
             
+            for (let e of [div, image]) {
+                e.setAttribute('dataid', dados[i].id)
+                e.setAttribute('draggable', true)
+                e.addEventListener('dragstart', drag)
+            }
+            image.addEventListener('dblclick', duploClick)
+
             div.setAttribute('class', 'column')
             div.appendChild(p)
             div.appendChild(image)
@@ -127,4 +141,87 @@ function limparProdutos () {
 
     document.getElementById('divProdutos').innerHTML = ""
 
+}
+
+function mostrarPagina(pagina) {
+    for (let p of document.getElementsByClassName('pagina')) {
+        p.style.display = "none"
+        if (p.id == `pagina${pagina}`) {
+            p.style.display = "block"
+        }
+    }
+}
+
+function duploClick(ev) {
+    carrinhoAdicionarItem(ev.target.getAttribute('dataid'))
+}
+
+function allowDrop(ev) {
+    ev.preventDefault()
+}
+
+function drop(ev) {
+    ev.preventDefault()
+    let data = ev.dataTransfer.getData("text")
+    carrinhoAdicionarItem(data)
+}
+
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.getAttribute('dataid'))
+}
+
+
+let carrinho = {}
+
+function mostrarCarrinho() {
+    let pagina = document.getElementById('paginaCarrinho')
+    pagina.style.display = pagina.style.display == "block" ? "none" : "block"
+}
+
+function gerarCarrinho() {
+    let items = `
+    <tr>
+        <th>Imagem</th>
+        <th>Nome</th>
+        <th>Quantidade</th>
+        <th>Alterar</th>
+        <th>Excluir</th>
+    </tr>`
+    for (let [id, quantidade] of Object.entries(carrinho)) {
+        item = produtos[id]
+        items += `
+        <tr id="modeloItem">
+            <td name="iconeItem"><img height="25px" src="${item.imagem}"></td>
+            <td name="nomeItem">${item.nome}</td>
+            <td name="quantidadeItem">${quantidade}</td>
+            <td>
+                <input type="number" name="alterarQuantidadeItem" onchange="carrinhoAlterarItem(event, ${id})" placeholder="${quantidade}">
+            </td>
+            <td>
+                <button name="excluirItem" style="position:static;" onclick="carrinhoExcluirItem(${id})">X</button>
+            </td>
+        </tr>`
+    }
+    document.getElementById("tabelaCarrinho").innerHTML = items
+}
+
+function carrinhoAdicionarItem(id) {
+    id = Number(id)
+    if ((id in produtos)) {
+        carrinho[id] = carrinho[id] + 1 || 1
+    }
+    gerarCarrinho()
+}
+
+function carrinhoAlterarItem(ev, id) {
+    carrinho[id] = Number(ev.target.value)
+    if (carrinho[id] <= 0) {
+        delete carrinho[id]
+    }
+    gerarCarrinho()
+}
+
+function carrinhoExcluirItem(id) {
+    delete carrinho[id]
+    gerarCarrinho()
 }
